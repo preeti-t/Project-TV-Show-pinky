5f36c1515f7a00bf24efe6253f756b4a6b507fa9
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-01 20:06:30 +0100

    change from .textContent to .innerHTMl till the end

--- script.js
+++ script.js
@@ -6,7 +6,31 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+
+  episodeList.forEach((episode) => {
+    const episodeCode = formatEpisodeCode(episode.season, episode.number);
+
+    const episodeCard = document.createElement("section");
+    episodeCard.className = "episode";
+
+    episodeCard.innerHTML = `
+      <h2>${episode.name} (${episodeCode})</h2>
+      <img src="${episode.image.medium}" alt="${episode.name}">
+      <div class="summary">
+        ${episode.summary}
+      </div>
+    `;
+
+    rootElem.appendChild(episodeCard);
+  });
+}
+
+function formatEpisodeCode(season, number) {
+  return `S${String(season).padStart(2, "0")}E${String(number).padStart(
+    2,
+    "0"
+  )}`;
 }
 
 window.onload = setup;
